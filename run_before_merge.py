f6fd2e3ba7eb910cc793799f42241d6d25bd0cb1
zvi_m <zvymendel@gmail.com>
2022-08-17 10:32:25 +0300

    Added mypy check

--- run_before_merge.py
+++ run_before_merge.py
@@ -1,17 +1,34 @@
 from subprocess import Popen, PIPE
-import subprocess
+from mypy import api
 
 # Constants
 REQUIREMENTS_FILE_NAME = "Requirements.txt"
 REQUIREMENTS_CMD = ["pip", "freeze", ">", REQUIREMENTS_FILE_NAME]
 
 
-def create_requirements_file() -> None:
+def create_requirements_file():
     with Popen(REQUIREMENTS_CMD, stdout=PIPE) as proc:
         requirements = proc.stdout.read()
     with open(REQUIREMENTS_FILE_NAME, 'wb') as f:
         f.write(requirements)
 
 
+def run_mypy_test() -> None:
+    print("-------- Starting mypy type checking --------")
+    result = api.run(["--check-untyped-defs", "../KMUtils"])
+
+    if result[0]:
+        print('\nType checking report:')
+        print(result[0])  # stdout
+
+    if result[1]:
+        print('\nError report:')
+        print(result[1])  # stderr
+
+    print('Exit status:', result[2])
+    print("-------- Finished mypy type checking --------")
+
+
 if __name__ == '__main__':
-    create_requirements_file()
+    # create_requirements_file()
+    run_mypy_test()
