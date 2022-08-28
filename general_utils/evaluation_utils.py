d0778a29520cbaef2a43d90f985b11e057b386bb
ZviLinux <zvymendel@gmail.com>
2022-08-28 13:18:14 +0300

    Removed eval_two_models - it is unnecessary

--- general_utils/evaluation_utils.py
+++ general_utils/evaluation_utils.py
@@ -13,30 +13,6 @@ DEFAULT_BATCH_SIZE = 32
 
 class EvaluationUtils(object):
     @staticmethod
-    def eval_two_models(model1: Module, model2: Module, dataset: Dataset,
-                        combine_func: Callable,
-                        accuracy_func: Callable, batch_size: DEFAULT_BATCH_SIZE,
-                        device: torch.device) -> float:
-        validation_loader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
-        model1.eval()
-        model2.eval()
-        total_num_images = 0
-        aggregate_accuracy = 0.0
-        for batch_num, (images, labels) in enumerate(validation_loader):
-            num_images_in_batch = images.shape[0]
-            total_num_images += num_images_in_batch
-            images = images.to(device)
-            labels = labels.to(device)
-            model1_output = model1(images)
-            model2_output = model2(images)
-            combined_output = combine_func(model1_output, model2_output)
-            current_accuracy = accuracy_func(combined_output, labels)
-            aggregate_accuracy += current_accuracy * num_images_in_batch
-
-        final_accuracy = aggregate_accuracy / total_num_images
-        return final_accuracy
-
-    @staticmethod
     def eval_model(model: Module, dataset: Dataset,
                    accuracy_func: Callable, batch_size: DEFAULT_BATCH_SIZE,
                    device: torch.device) -> float:
@@ -60,9 +36,7 @@ class EvaluationUtils(object):
         pass
 
 
-# Code to evaluate eval_two_models func
-
-
+# Code to evaluate "eval_model" func
 class RandomDataSet(Dataset):
     def __len__(self):
         return 100
@@ -107,21 +81,10 @@ def combine_outputs(output1: torch.tensor, output2: torch.tensor) -> torch.tenso
     return output1_padded + output2
 
 
-def two_model_accuracy(output1: torch.tensor, output2: torch.tensor, label: torch.tensor) -> float:
-    combined_output = combine_outputs(output1, output2)
-    return label_accuracy(combined_output, label)
-
-
 if __name__ == '__main__':
     random_dataset = RandomDataSet()
     first_model = Model1()
     second_model = Model2()
-    accuracy = EvaluationUtils.eval_two_models(first_model, second_model, dataset=random_dataset,
-                                               accuracy_func=label_accuracy,
-                                               combine_func=combine_outputs,
-                                               batch_size=32, device=torch.device('cpu'))
-    print(f"final accuracy of 'eval_two_models' {accuracy}")
-
     combined_model = ModelCombiner(first_model, second_model, combine_outputs)
     accuracy = EvaluationUtils.eval_model(combined_model, dataset=random_dataset,
                                           accuracy_func=label_accuracy,
