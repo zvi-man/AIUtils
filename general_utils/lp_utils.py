d7a640f320b571a1cef8d2bb3f7bb32a25c1be9e
ZviLinux <zvymendel@gmail.com>
2022-08-28 14:24:31 +0300

    added_lp_utils

---
+++ general_utils/lp_utils.py
@@ -0,0 +1,34 @@
+import re
+from typing import List
+
+# Constants
+DEFAULT_LABEL: str = "AAAAAA"
+DEFAULT_DELIMITER_PAT: str = r"-|_|\."
+ACCEPTABLE_FILE_TYPES: List[str] = ["jpg", "bmp"]
+ID_LOCATION_IN_FILE_NAME: int = 2
+LABEL_LOCATION_IN_FILE_NAME: int = 3
+
+
+class LPUtils(object):
+    def __init__(self, delimiter_pat: str = DEFAULT_DELIMITER_PAT,
+                 id_location_in_file_name: int = ID_LOCATION_IN_FILE_NAME,
+                 label_location_in_file_name: int = LABEL_LOCATION_IN_FILE_NAME):
+        self.delimiter_pat = delimiter_pat
+        self.id_location_in_file_name = id_location_in_file_name
+        self.label_location_in_file_name = label_location_in_file_name
+
+    def get_label_from_file_name(self, file_name: str) -> str:
+        old_name_fields = re.split(self.delimiter_pat, file_name)
+        return old_name_fields[self.label_location_in_file_name]
+
+    def get_id_from_file_name(self, file_name: str) -> int:
+        old_name_fields = re.split(self.delimiter_pat, file_name)
+        id_str = old_name_fields[self.id_location_in_file_name]
+        id_int = -1 if id_str == "UN" else int(id_str)
+        return id_int
+
+    def create_new_labeled_file_name(self, old_name: str, label: str) -> str:
+        old_label = self.get_label_from_file_name(old_name)
+        if old_name.count(old_label) != 1:
+            raise NotImplementedError("File name should have only one occurrence of label")
+        return old_name.replace(old_label, label)
