bea1be80cb52b70b3296c79382d16dc8179052e0
zvi_m <zvymendel@gmail.com>
2023-01-08 13:02:20 +0200

    Added plot multiple precision recall curves

--- GeneralUtils/plot_utils.py
+++ GeneralUtils/plot_utils.py
@@ -1,20 +1,52 @@
-from typing import Optional, Tuple, List
-
+from typing import Optional, Tuple, List, Dict
+from matplotlib.colors import cnames
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 
 
 class PlotUtils(object):
     @staticmethod
-    def plot_precision_recall_curve(precision_list: List[float], recall_list: List[float],
-                                    title: str = "Precision-Recall Curve",
-                                    axes: Optional[plt.Axes] = None, do_show: bool = True) -> Optional[plt.Axes]:
+    def get_random_n_colors(n: int) -> List[str]:
+        return random.sample(set(cnames.keys()), n)
+
+    @classmethod
+    def plot_multiple_precision_recall_curves(cls, precision_recall_curves_dict: Dict[str, Tuple[List[float], List[float]]],
+                                              title: str = "Precision-Recall Curve",
+                                              axes: Optional[plt.Axes] = None, do_show: bool = True) -> Optional[plt.Axes]:
+        """
+        :param precision_recall_curves_dict:
+            keys = name of precision_recall_curve,
+            values = list of precision values and list of recall values
+        """
         if not axes:
             _, axes = plt.subplots(1)
-        plt.plot(recall_list, precision_list)
+        num_of_curves = len(precision_recall_curves_dict)
+        colors = cls.get_random_n_colors(num_of_curves)
+        for curve_name in precision_recall_curves_dict:
+            precision_list = precision_recall_curves_dict[curve_name][0]
+            recall_list = precision_recall_curves_dict[curve_name][1]
+            cls.plot_precision_recall_curve(precision_list, recall_list, title=None,
+                                            colors=colors.pop(), axes=axes, do_show=False)
         plt.xlabel("Recall")
         plt.ylabel("Precision")
         plt.title(title)
+        if do_show:
+            plt.show()
+        return axes
+
+    @staticmethod
+    def plot_precision_recall_curve(precision_list: List[float], recall_list: List[float],
+                                    title: str = "Precision-Recall Curve",
+                                    color: str = None,
+                                    axes: Optional[plt.Axes] = None, do_show: bool = True) -> Optional[plt.Axes]:
+        if not axes:
+            _, axes = plt.subplots(1)
+        plt.plot(recall_list, precision_list, c=color, marker='x')
+        plt.xlabel("Recall")
+        plt.ylabel("Precision")
+        if title:
+            plt.title(title)
         if do_show:
             plt.show()
         return axes
